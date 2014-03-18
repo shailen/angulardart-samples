ea6622d75d396e0485db712579e135e33c73ddb1
Shailen Tuli <shailen.tuli@gmail.com>
2014-03-17 17:34:46 -0700

    Added example of a simple directive

---
+++ mini_samples/example/directives/extending_element_behavior_using_a_directive/my_directive.dart
@@ -0,0 +1,35 @@
+library my_directive;
+
+import 'package:angular/angular.dart';
+
+import 'dart:html' show Element, InputElement;
+
+@NgDirective(selector: '[is-palindrome]')
+class MyDirective implements NgAttachAware{
+
+  Element element;
+
+  MyDirective(this.element);
+
+  attach(){
+    element.onKeyUp.listen((_) {
+      if (isPalindrome((element as InputElement).value)) {
+        element.classes.add('palindrome');
+      } else {
+        element.classes.remove('palindrome');
+      }
+    });
+  }
+
+  bool isPalindrome(value) {
+    if (value.isEmpty || value.length == 1) return false;
+
+    var buffer = new StringBuffer();
+    var lcText = value.toLowerCase();
+
+    for (int i = lcText.length - 1; i >= 0; i--) {
+      buffer.write(lcText[i]);
+    }
+    return lcText == buffer.toString();
+  }
+}
