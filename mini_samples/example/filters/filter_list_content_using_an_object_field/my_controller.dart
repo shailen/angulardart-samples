793871c523740dcc59a3dca34042a1e847324c3e
Shailen Tuli <shailen.tuli@gmail.com>
2014-03-27 15:53:17 -0700

    Added filterfilter example

---
+++ mini_samples/example/filters/filter_list_content_using_an_object_field/my_controller.dart
@@ -0,0 +1,27 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library my_controller;
+
+import 'package:angular/angular.dart';
+
+class User {
+  String name;
+  bool registered;
+  User(this.name, [this.registered=false]);
+}
+
+@NgController(
+    selector: '[my-controller]',
+    publishAs: 'ctrl'
+)
+class MyController {
+  List<User> users = [
+    new User('Anna', true),
+    new User('Hannah'),
+    new User('Mary'),
+    new User('Mem'),
+    new User('Otto'),
+  ];
+}
